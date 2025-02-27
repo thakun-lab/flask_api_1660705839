f1e180c5a8f4dbdaa86c71aa3bddd73ba1426f0b
thakun-lab <thakun.buna@bumail.net>
2025-02-27 21:21:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, BU 1660705839'
+    return 'Hello, BU3-1660705839'
 
 if __name__ == '__main__':
     app.run(debug=True)
